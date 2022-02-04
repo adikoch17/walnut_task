763bed71a3006b50aeb9f274c806ae4cc7aa34be
aditya kocherlakota <aditya.kocherlakota@gmail.com>
2022-02-04 21:43:56 +0530

    completed

---
+++ walnut_task/src/App.js
@@ -0,0 +1,86 @@
+import {useState} from 'react'
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [age,setAge] = useState("")
+  const [day,setDay] = useState(0)
+  const [month,setMonth] = useState(0)
+  const [year,setYear] = useState(0)
+  const [error,setError] = useState("")
+
+  const calculateAge = () =>{
+    setAge("")
+    setError("")
+    let a = 0;
+    let d = new Date(Date.now())
+    let day_today = d.getDate()
+    let month_today = d.getMonth()+1
+    let year_today = d.getFullYear()
+
+    if(parseInt(year)>year_today){
+      setError("Year cannot be greater than the current one")
+    }
+    else if(parseInt(day)<=0 || parseInt(month)<=0 || parseInt(year)<=0){
+      setError("Invalid input")
+    }
+    else{
+      if(parseInt(year)===year_today && parseInt(month)>month_today){
+        setError("Month cannot be greater than the current one")
+      }
+      else{
+        if(parseInt(month)===month_today && parseInt(day)>day_today){
+          setError("Day cannot be greater than the current one")
+        }
+        else{
+          a = year_today-parseInt(year)
+          if(parseInt(year)===year_today){
+            a = 0
+          }
+          else{ 
+            if(parseInt(month)===month_today){
+              if(parseInt(day)<day_today){
+                a = a-1
+              }
+            }
+            else if(parseInt(month)>month_today){
+              a=a-1
+            }
+          } 
+        }
+      }  
+    }
+    if(error===""){
+      setAge(`Age: ${a} `)
+    }
+    else{
+      setAge("")
+    }
+  }
+
+function onClickGetAge(){
+    calculateAge()
+  }
+
+  return (
+    <div className="App">
+      <h3>Age calculator</h3>
+      <div className='container'>
+        <label>Day</label>
+        <input type="number" onChange={e=>{setDay(e.target.value)}}/>
+        <label>Month</label>
+        <input type="number" onChange={e=>{setMonth(e.target.value)}}/>
+        <label>Year</label>
+        <input type="number" onChange={e=>{setYear(e.target.value)}}/>
+        <button onClick={onClickGetAge}>Get Age</button>
+      </div>
+      <p>
+          {age}
+          {error}
+        </p>
+    </div>
+  );
+}
+
+export default App;
